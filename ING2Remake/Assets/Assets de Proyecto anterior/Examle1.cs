f8cbf38fc6b322cd039c82e121687971526c7a03
Maez H. Aguilar V <maez-aguilar@hotmail.com>
2018-07-11 22:03:50 -0500

    Comit pregunta DB

--- ING2Remake/Assets/Assets de Proyecto anterior/Examle1.cs
+++ ING2Remake/Assets/Assets de Proyecto anterior/Examle1.cs
@@ -2,10 +2,18 @@
 using System.Data;
 using Mono.Data.Sqlite;
 using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+
 
 public class Examle1 : MonoBehaviour
-{
+{ 
+    GameObject[] CiertoS, FalsoS;
+    public GameObject correctSign, incorrectSign;
     public TextMesh PBDT;
+    public int puntosGanados = 1;
+    public int puntosPerdidos = 1;
+    public int Intentos = 3;
+
     int IDP;
     string Pregunta;
     string Respuesta1;
@@ -13,27 +21,29 @@ public class Examle1 : MonoBehaviour
     string Respuesta3;
     void Start()
     {
-        NotificationCenter.DefaultCenter().AddObserver(this, "Pregunta");
-        PBD();
+
+        CiertoS = GameObject.FindGameObjectsWithTag("CiertoS");
+        FalsoS = GameObject.FindGameObjectsWithTag("FalsoS");
         string conn = "URI=file:" + Application.dataPath + "/PDB.s3db"; //Path to database.
         IDbConnection dbconn;
         dbconn = (IDbConnection)new SqliteConnection(conn);
         dbconn.Open(); //Open connection to the database.
         IDbCommand dbcmd = dbconn.CreateCommand();
         //string sqlQuery = "SELECT IDP, Pregunta, Respeusta1, Respeusta2, Respeusta3 " + "FROM Preguntas";
-        string sqlQuery = "SELECT IDP " + "FROM Preguntas";
+        string sqlQuery = "SELECT Pregunta, Respuesta1 , Respuesta2 , Respuesta3 " + "FROM Preguntas";
         dbcmd.CommandText = sqlQuery;
         IDataReader reader = dbcmd.ExecuteReader();
         while (reader.Read())
         {
-            IDP = reader.GetInt32(0);
-            Pregunta = reader.GetString(1);
-            Respuesta1 = reader.GetString(2);
-            Respuesta2 = reader.GetString(3);
-            Respuesta3 = reader.GetString(4);
-            PBDT.text = IDP.ToString();
-            //Debug.Log("IDPregunta= " + IDP + "  Pregunta =" + Pregunta + "  Respuesta1 =" + Respuesta1 + "  Respuesta2 =" + Respuesta2 +"  Respuesta3 =" + Respuesta3);
-            Debug.Log("IDPregunta= " + IDP);
+            //IDP = reader.GetInt32(0);
+            Pregunta = reader.GetString(0);
+            Respuesta1 = reader.GetString(1);
+            Respuesta2 = reader.GetString(2);
+            Respuesta3 = reader.GetString(3);
+            PBDT.text = Pregunta;
+            Debug.Log("Pregunta =" + Pregunta + "  Respuesta1 =" + Respuesta1 + "  Respuesta2 =" + Respuesta2 +"  Respuesta3 =" + Respuesta3);
+           
+
         }
         reader.Close();
         reader = null;
@@ -42,14 +52,65 @@ public class Examle1 : MonoBehaviour
         dbconn.Close();
         dbconn = null;
     }
-    /* void Prueba(Notification notificacion)
-     {
-         int IDPREGUNTA = (int)notificacion.data;
-         idp = IDPREGUNTA;
-         PBD();
-     }*/
-    void PBD()
+
+    void OnGUI()
     {
-        PBDT.text = IDP.ToString();
+        if (GUI.Button(new Rect(Screen.width -765, Screen.height / 2, 150, 40), Respuesta1))
+        {
+            try
+            {
+                foreach (GameObject element in CiertoS)
+                {
+                    element.gameObject.SetActive(true);
+                }
+                correctSign.gameObject.SetActive(true);
+                NotificationCenter.DefaultCenter().PostNotification(this, "IncrementarPuntos", puntosGanados);
+                Debug.Log("Respuesta Correcta, ha ganado 5 puntos Pase a la Siguiente Pregunta");
+                NotificationCenter.DefaultCenter().PostNotification(this, "GameOver", Intentos);
+
+            }
+            catch (System.NullReferenceException ex)
+            {
+                Debug.Log("ERROR DE VARIABLE NOINSTANCIADAOREFERENCIADA" + ex);
+            }
+        }
+        if (GUI.Button(new Rect(Screen.width - 765 , Screen.height -250 , 150, 40), Respuesta2))
+        {
+            try
+            {
+                foreach (GameObject element in FalsoS)
+                {
+                    element.gameObject.SetActive(true);
+                }
+                incorrectSign.gameObject.SetActive(true);
+                NotificationCenter.DefaultCenter().PostNotification(this, "ReducirPuntos", puntosPerdidos);
+                Debug.Log("Respuesta Incorrecta, ha perdido 5 puntos Pase a la Siguiente Pregunta");
+                NotificationCenter.DefaultCenter().PostNotification(this, "GameOver", Intentos);
+
+            }
+            catch (System.NullReferenceException ex)
+            {
+                Debug.Log("ERROR DE VARIABLE NOINSTANCIADAOREFERENCIADA" + ex);
+            }
+        }
+        if (GUI.Button(new Rect(Screen.width - 765, Screen.height - 200, 150, 40), Respuesta3))
+        {
+            try
+            {
+                foreach (GameObject element in FalsoS)
+                {
+                    element.gameObject.SetActive(true);
+                }
+                incorrectSign.gameObject.SetActive(true);
+                NotificationCenter.DefaultCenter().PostNotification(this, "ReducirPuntos", puntosPerdidos);
+                Debug.Log("Respuesta Incorrecta, ha perdido 5 puntos Pase a la Siguiente Pregunta");
+                NotificationCenter.DefaultCenter().PostNotification(this, "GameOver", Intentos);
+
+            }
+            catch (System.NullReferenceException ex)
+            {
+                Debug.Log("ERROR DE VARIABLE NOINSTANCIADAOREFERENCIADA" + ex);
+            }
+        }
     }
 }
