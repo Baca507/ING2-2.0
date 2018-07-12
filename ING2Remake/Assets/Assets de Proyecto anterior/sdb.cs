973f553f93a0d0486d63003a91b0425909a174a6
Maez H. Aguilar V <maez-aguilar@hotmail.com>
2018-07-12 02:43:40 -0500

    -----no reconoce tabla perfil

--- ING2Remake/Assets/Assets de Proyecto anterior/sdb.cs
+++ ING2Remake/Assets/Assets de Proyecto anterior/sdb.cs
@@ -4,20 +4,30 @@ using UnityEngine;
 using Mono.Data.Sqlite;
 using System.Data;
 using System;
-public class insert : MonoBehaviour {
+using UnityEngine.UI;
+using System.Text.RegularExpressions;
+public class sdb : MonoBehaviour {
+    public GameObject usuario, password;
     private string conn, sqlQuery;
     IDbConnection dbconn;
     IDbCommand dbcmd;
-	// Use this for initialization
-	void Start () {
-        conn = "URI=file:" + Application.dataPath + "/Plugins/Users.s3db"; //Path to database.
+    private string Usuario;
+    private string Password;
+    private String[] Lines;
+    private string DecryptedPass;
+    private string pass;
+    private string name;
+    string contra = "8888";
+    // Use this for initialization
+    void Start () {
+        conn = "URI=file:" + Application.dataPath + "/PDB.s3db"; //Path to database.
         //Deletvalue(6);
         //insertvalue("ahmedm", "ahmedm@gmail.com", "sss"); 
-        Updatevalue("a","w@gamil.com","1st",1);
+        //Updatevalue("Prueba", contra, 31 / 03 / 1992, "Prueba@hotmail.com", 8888);
         readers();
 	}
 
-    private void insertvalue(string name, string email, string address)
+    /*private void insertvalue(string name, string email, string address)
     {
         using (dbconn = new SqliteConnection(conn))
         {
@@ -40,41 +50,45 @@ public class insert : MonoBehaviour {
             dbcmd.ExecuteScalar();
             dbconn.Close();
         }
-    }
+    }*/
 
 
-    private void Updatevalue(string name, string email, string address,int id)
+   /* private void Updatevalue(string name, string pass, SQLiteDateFormats Fna, string email, int cedula)
     {
         using (dbconn = new SqliteConnection(conn))
         {
 
             dbconn.Open(); //Open connection to the database.
             dbcmd = dbconn.CreateCommand();
-            sqlQuery = string.Format("UPDATE Usersinfo set Name=\"{0}\", Email=\"{1}\", Address=\"{2}\" WHERE ID=\"{3}\" ", name, email, address, id);// table name
+            sqlQuery = string.Format("UPDATE Perfil set Nombre_Completo=\"{0}\", Password=\"{1}\",FNacimiento=\"{2}\", CorreoE=\"{3}\" WHERE ID=\"{4}\" ", name,pass, Fna, email, cedula);// table name
             dbcmd.CommandText = sqlQuery;
             dbcmd.ExecuteScalar();
             dbconn.Close();
         }
-    }
+    }*/
 
-
+    
     private void readers()
     {
         using (dbconn = new SqliteConnection(conn))
         {
             dbconn.Open(); //Open connection to the database.
             dbcmd = dbconn.CreateCommand();
-            sqlQuery = "SELECT * " + "FROM Usersinfo";// table name
+            sqlQuery = "SELECT Cedula, Nombre_Completo, Password, FNacimiento, CorreoE " + "FROM Perfil";// table name
             dbcmd.CommandText = sqlQuery;
             IDataReader reader = dbcmd.ExecuteReader();
             while (reader.Read())
             {
-                int id = reader.GetInt32(0);
-                string name = reader.GetString(1);
-                string Email = reader.GetString(2);
-                string Phone = reader.GetString(3);
+                int cedu = reader.GetInt32(0);
+                name = reader.GetString(1);
+                pass = reader.GetString(2);
+                string FN = reader.GetString(3);
+                string email = reader.GetString(4);
+                
 
-                Debug.Log("value= " + id + "  name =" + name + "  Eamil =" + Email + "   Phone" + Phone);
+                Debug.Log("Cedula= " + cedu + "  Nombre Completo =" + name + "  Password =" + pass + "  Fecha de nacimiento =" + FN + "   Correo Electronico" + email);
+                
+                
             }
             reader.Close();
             reader = null;
@@ -84,9 +98,97 @@ public class insert : MonoBehaviour {
             dbconn = null;
         }
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public void LoginButton()
+    {
+        bool UN = false;
+        bool PW = false;
+        if (Usuario != "")
+        {
+            if (Usuario.Equals(name))
+            {
+                UN = true;
+                Debug.Log("Usuario Validado");
+            }
+            else
+            {
+                Debug.LogWarning("Username Invaild");
+            }
+        }
+        else
+        {
+            Debug.LogWarning("Username Field Empty");
+        }
+        if (Password != "")
+        {
+            if (Password.Equals(pass))
+            {
+                
+                    PW = true;
+                    Debug.Log("Password Validado");
+                
+            }
+            else
+            {
+                Debug.LogWarning("Password Is invalid");
+            }
+        }
+        else
+        {
+            Debug.LogWarning("Password Field Empty");
+        }
+        if (UN == true && PW == true)
+        {
+            usuario.GetComponent<InputField>().text = "";
+            password.GetComponent<InputField>().text = "";
+            print("Login Sucessful");
+            //Application.LoadLevel("Start Menu");
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (usuario.GetComponent<InputField>().isFocused)
+            {
+                password.GetComponent<InputField>().Select();
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if (Password != "" && Password != "")
+            {
+                LoginButton();
+            }
+        }
+        Usuario = usuario.GetComponent<InputField>().text;
+        Password = password.GetComponent<InputField>().text;
+    }
+
+/*private void ConsultaVali()
+    {
+        using (dbconn = new SqliteConnection(conn))
+        {
+            dbconn.Open(); //Open connection to the database.
+            dbcmd = dbconn.CreateCommand();
+            sqlQuery = "SELECT * " + "FROM Perfil";// table name
+            dbcmd.CommandText = sqlQuery;
+            IDataReader reader = dbcmd.ExecuteReader();
+            while (reader.Read())
+            {
+                int cedu = reader.GetInt32(0);
+                string name = reader.GetString(1);
+                string Email = reader.GetString(2);
+                string Phone = reader.GetString(3);
+
+                Debug.Log("value= " + cedu + "  name =" + name + "  Eamil =" + Email + "   Phone" + Phone);
+            }
+            reader.Close();
+            reader = null;
+            dbcmd.Dispose();
+            dbcmd = null;
+            dbconn.Close();
+            dbconn = null;
+        }
+    }*/
 }
