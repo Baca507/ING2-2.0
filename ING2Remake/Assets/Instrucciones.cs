2e597fcbe6a3639536779ecbcaeffee1b85d66a4
Edgar <baca507@gmail.com>
2018-07-12 01:05:22 -0500

    instrucciones mapa

---
+++ ING2Remake/Assets/Instrucciones.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instrucciones : MonoBehaviour {
+
+	public bool displayMessage = true;
+	public string message;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnGUI () {
+	
+     if (displayMessage) {
+         GUI.Box(new Rect (380,200,540,50), message);
+
+		 if(GUI.Button(new Rect(570,300,150,20), "OK")) {
+			 displayMessage = false;
+        
+        }
+     }
+	
+ }
+}
